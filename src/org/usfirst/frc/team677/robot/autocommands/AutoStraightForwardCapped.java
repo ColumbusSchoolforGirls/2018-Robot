3383b94f80e2d02ba3cd2e20d0c8f4a1872f4e15
Evelyn Stuntz <estuntz@columbusschoolforgirls.org>
2018-03-28 18:22:48 -0400

    Auto Straight Forward Capped
    Created a version of the forward auto command with speed caps.
    
    Co-Authored-By: ganthony4 <ganthony4@users.noreply.github.com>

---
+++ src/org/usfirst/frc/team677/robot/autocommands/AutoStraightForwardCapped.java
@@ -0,0 +1,80 @@
+package org.usfirst.frc.team677.robot.autocommands;
+
+import org.usfirst.frc.team677.robot.Global;
+import org.usfirst.frc.team677.robot.PIDCalculator;
+import org.usfirst.frc.team677.robot.Robot;
+import org.usfirst.frc.team677.robot.subsystems.Drivetrain;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class AutoStraightForwardCapped extends Command {
+	private double leftError;
+	private double rightError;
+	private double angle;
+	private double setpoint;
+	private PIDCalculator distPID;
+	private PIDCalculator anglePID;
+
+    public AutoStraightForwardCapped(double ticks) {
+    	requires(Robot.drivetrain);
+    	setpoint = ticks; //When calling this method, ticks should be one of the Global constants
+    	distPID = new PIDCalculator(Global.DRIVETRAIN_P, Global.DRIVETRAIN_I, Global.DRIVETRAIN_D); //TODO: Tune this
+    	anglePID = new PIDCalculator(Global.DRIVESTRAIGHT_ANGLE_P, Global.DRIVESTRAIGHT_ANGLE_I, Global.DRIVESTRAIGHT_ANGLE_D);
+    	Drivetrain.setSpeed(ControlMode.PercentOutput, 0, 0);
+    }
+
+    protected void initialize() {
+    	Drivetrain.resetEncoders();
+    	angle = Drivetrain.getFacingAngle();
+    }
+
+    protected void execute() {
+    	leftError = setpoint - Drivetrain.getLeftEncoder();
+    	rightError = setpoint - Drivetrain.getRightEncoder();
+    	double angleError = angle - Drivetrain.getFacingAngle();
+    	
+    	double leftOutput = distPID.getOutput(leftError);
+    	double rightOutput = distPID.getOutput(rightError);
+    	double angleOutput = anglePID.getOutput(angleError);
+    	
+    	SmartDashboard.putNumber("Left Error", leftError);    	
+    	SmartDashboard.putNumber("Right Error", rightError);
+    	
+    	if (leftOutput > .7) {
+    		leftOutput = .7; 
+    	} else if (leftOutput < -.7) {
+    		leftOutput = -.7;
+    	}
+    	
+    	if (rightOutput > .7) {
+    		rightOutput = .7;
+    	} else if (rightOutput < -.7) {
+    		rightOutput = -.7;
+    	}
+    	
+    	double leftSpeed = -leftOutput - angleOutput;
+    	double rightSpeed = -rightOutput + angleOutput;
+    	
+    	Drivetrain.setSpeed(ControlMode.PercentOutput, leftSpeed, rightSpeed);
+    }
+
+    protected boolean isFinished() {
+        return Math.abs(leftError) <= Global.DRIVE_DISTANCE_TOLERANCE;
+    }
+
+    protected void end() {
+    	Drivetrain.setSpeed(ControlMode.PercentOutput, 0, 0);
+//    	Drivetrain.resetEncoders();    	
+    }
+
+    protected void interrupted() {
+    }
+   
+}
+
